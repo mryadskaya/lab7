b15a7a416b5b9ae437e57a222728103a414d307d
mryadskaya <125180218+mryadskaya@users.noreply.github.com>
2023-11-22 09:44:43 +0300

    Update zd1.py

--- PyCharm/zd1.py
+++ PyCharm/zd1.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
         exit(1)
 
     # Найтии и вывести сумму элементов
-    s = sum([a for a in A if a > 3 and a < 8 and i % 10 == 0] )
+    s = sum([a for a in A if a > 3 and a < 8 and a % 10 == 0] )
     print(s)
